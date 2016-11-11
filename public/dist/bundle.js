3d087ab51e7d868404d08d3451ff7bce4af7bac8
Jacob <admin@Mjolnir.local>
2016-11-10 23:59:41 -0700

    sxs page almonst done

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -274,7 +274,65 @@ angular.module('personalPro').controller('registerCtrl', ["$scope", function($sc
 
 angular.module('personalPro').controller('sxsCtrl', ["$scope", function($scope){
 
+  $(window).scroll(function(){
 
+    var winScroll = $(this).scrollTop();
+
+    if(winScroll > $('.image-flow-container').offset().top - ($(window).height() / 2)) {
+        $('.image-flow').each(function(i){
+          setTimeout(function(){
+            $('.image-flow').eq(i).addClass('is-showing');
+          }, 150 * (i+1));
+        });
+      }
+  });
+  $(window).scroll(function(){
+
+    var winScroll = $(this).scrollTop();
+
+    if(winScroll > $('.image-fuel-container').offset().top - ($(window).height() / 2)) {
+        $('.image-fuel').each(function(i){
+          setTimeout(function(){
+            $('.image-fuel').eq(i).addClass('is-fuel');
+          }, 150 * (i+1));
+        });
+      }
+  });
+  var myIndex = 0;
+  carousel();
+
+  function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}
+      x[myIndex-1].style.display = "block";
+      setTimeout(carousel, 4000); // Change image every 2 seconds
+  }
+
+  $(function(){
+    $("#redBtnH").on("click",function(){
+      $(".default").css({"background-image": "url('https://powersports.honda.com/images/HL4/color-1000/Pioneer1000-5_DLX_Red.png')"});
+    });
+  });
+  $(function(){
+    $("#whiteBtnH").on("click",function(){
+      $(".default").css({"background-image": "url('https://powersports.honda.com/images/HL4/color-1000/Pioneer1000-5_DLX_White.png')"});
+    });
+  });
+  $(function(){
+    $("#greyBtnH").on("click",function(){
+      $(".default").css({"background-image": "url('https://powersports.honda.com/images/HL4/color-1000/Pioneer1000-5_DLX_MetallicSilver.png')"});
+    });
+  });
+  $(function(){
+    $("#camoBtnH").on("click",function(){
+      $(".default").css({"background-image": "url('https://powersports.honda.com/images/HL4/color-1000/Pioneer1000-5_DLX_HondaPhantomCamo.png')"});
+    });
+  });
 
 }])
 
